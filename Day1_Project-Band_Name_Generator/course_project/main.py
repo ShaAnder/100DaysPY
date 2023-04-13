b64bb91e23f05b6c9ba21250a8977087e1579e31
ShaAnder <shaunanderton1@gmail.com>
2023-04-13 23:43:12 +0100

    Added day one course project

---
+++ Day1_Project-Band_Name_Generator/course_project/main.py
@@ -0,0 +1,19 @@
+#Step1:
+print("This Is The Band Name Generator Hit Me With Your Suggestions!")
+
+#Step2:
+home_town = input("Let's start simple my guy where did you grow up?:\n")
+
+print("Heyyy nice place always wanted to go")
+
+#Step3:
+users_pet = input("Ok so I need a name, I got it! How about your pet's name?:\n")
+
+print("Awesome, let's whip up that name shall we!")
+
+#Step4:
+band_name_1 = home_town + " " + users_pet
+band_name_2 = users_pet + " " + home_town
+
+#Step5: applied to both inputs
+print("Your band name could be: "+ band_name_1 +"! or maybe "+ band_name_2 +"!\nWhat do you think?")
