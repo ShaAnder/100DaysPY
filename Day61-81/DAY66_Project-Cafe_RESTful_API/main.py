45583999fe5e621fd2036239facdb584656bda78
ShaAnder <shaunanderton1@gmail.com>
2023-06-08 18:52:44 +0100

    Day 66 Cafe_RESTful_API project complete

--- Day61-81/DAY66_Project-Cafe_RESTful_API/main.py
+++ Day61-81/DAY66_Project-Cafe_RESTful_API/main.py
@@ -7,6 +7,11 @@ from flask_sqlalchemy import SQLAlchemy
 #we want to get this for getting a random line in the db
 import random
 
+### --- VARS --- ###
+
+API_KEY = "TopSecretAPIKey"
+
+
 ### --- APP & DB SETUP --- ###
 
 app = Flask(__name__)
@@ -32,43 +37,118 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
+    def to_dict(self):
+        """Returns the elements of the object as key value pairs"""
+    # use dictionary comprehension, Key(column name): Value(attribute in the column name) for the column in the table
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+        #we use this for later as some of our data comes back as a list of objects and we want to convert that into a dict
+        #tldr this method goes through the list object, extracts the info converts it into a key value pair and saves it into a dict
+
 ### --- FUNCS --- ###
 
 @app.route("/")
 def home():
     return render_template("index.html")
     
-## HTTP GET - Read Record
-
-#get is allowed by default, so don't need to add the methods unless we want to post
 @app.route("/random")
 def get_random_cafe():
-    # return random_cafe
+    """Gets A Random Cafe"""
+    #Get our data
     cafes = db.session.query(Cafe).all()
+    #Get a random cafe
     random_cafe = random.choice(cafes)
-    #using dict comprehension we cut down all the typing of declaring all the info for a json
-    #this code here says get the attributes for each item of the cafe in the random cafe
-    json_cafe = {item: getattr(random_cafe, item) for item in Cafe.__table__.columns.keys()}
-    return jsonify(cafe=json_cafe)
+    #return the cafe as a dict
+    return jsonify(cafe=random_cafe.to_dict())
 
 @app.route("/all")
 def get_all_cafes():
+    """Gets All The Cafe's In The DB"""
+    #we query all the cafes
     cafes = db.session.query(Cafe).all()
-    json_all_cafe = []
-    for cafe in cafes:
-        json_cafe = {item: getattr(cafe, item) for item in Cafe.__table__.columns.keys()}
-        json_all_cafe.append(json_cafe)
-    return jsonify(cafe=json_all_cafe)
+    #then using our to_dict method, as well as list comprehension, convert the returned data into
+    #a dict and then append it to a list
+    return jsonify(cafes= [cafe.to_dict() for cafe in cafes])
 
 @app.route("/search")
 def search_for_cafe():
-    pass
+    """Allows You To Search For A Cafe"""
+    #we want to request the location arg for our cafes
+    location = request.args.get("location").title()
+    #we query the cafe DB and filter by all the locations
+    requested_cafes = Cafe.query.filter_by(location=location).all()
+    #if there are cafes in the list
+    if requested_cafes:
+        #and here we put our to dict method into action. We convert each entry in the cafe to a dict using
+        #the method and do this for all the cafes, then return them as a json'd dict appended to the cafes list
+        return jsonify(cafes = [cafe.to_dict() for cafe in requested_cafes])
+    return jsonify(error={"Sorry there are no cafes in that location"})
 
-# HTTP POST - Create Record
+@app.route("/add", methods=["POST"])
+def add_a_cafe():
+    """Adds A New Cafe"""
+    #we're adding a new cafe, we take the request args from the POST request from postman
+    #then we feed them into the new Cafe Object
+    new_cafe = Cafe(
+        name = request.args.get("name"),
+        map_url=request.args.get("map_url"),
+        img_url=request.args.get("img_url"),
+        location=request.args.get("location"),
+        seats=request.args.get("seats"),
+        #these 4 are booleans so we want a true false statement
+        has_toilet=bool(request.args.get("toilet")),
+        has_wifi=bool(request.args.get("wifi")),
+        has_sockets=bool(request.args.get("sockets")),
+        can_take_calls=bool(request.args.get("calls")),
+        coffee_price=request.args.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    if new_cafe:
+        return jsonify(response = {"success": "Successfully added new cafe"})
+    return jsonify(response = {"failure": "New cafe could note be added"})
 
-## HTTP PUT/PATCH - Update Record
+#we want to update coffee prices with a patch request
+@app.route("/update-price/<cafe_id>", methods = ["GET","POST","PATCH"])
+def update(cafe_id):
+    """Updates The Cafe Listing"""
+    #get our cafe, we filter by the id, the user will get this via the get request (we can also change it to name later)
+    requested_cafe = Cafe.query.filter_by(id=cafe_id).first()
+    #setup a conditional to make sure we don't try and add to a non existent cafe
+    if requested_cafe is not None:
+        #get the new price request from postman
+        new_price = request.args.get("new_price")
+        #then change the price
+        requested_cafe.coffee_price = f"£{new_price}"
+        #commit the session
+        db.session.commit()
+        #return info
+        return jsonify(response={"success": "Successfully updated the price."}), 200
+    return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
 
-## HTTP DELETE - Delete Record
+#we want to delete records of closed cafes will be using the test cafe i made in step 6
+@app.route("/report-closed/<cafe_id>", methods = ["GET","DELETE"])
+def delete_cafe(cafe_id):
+    """Deletes a closed cafe"""
+    api = request.args.get("api-key")
+    #we have this for testing (this is to test and ensure that if the api key IS present code works)
+    #we do this because the request is coming from postman and not our browser and will always fail if we try 
+    #refresh our code and not send from the postman app
+    # api = "TopSecretAPIKey"
+    if api == "TopSecretAPIKey":
+        #get the first entry with our id
+        requested_cafe = Cafe.query.filter_by(id=cafe_id).first()
+        #if it gets the entry
+        if requested_cafe:
+            #delete
+            db.session.delete(requested_cafe)
+            db.session.commit()
+    #-------------- if / else response handling --------- #
+            return jsonify(response={"success": "Successfully removed the cafe from the database."}), 200
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found."}), 404
+    else:
+        return jsonify(error={"Forbidden": "Sorry, that's not allowed. Make sure your api_key is correct."}), 403
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Day61-81/DAY66_Project-Cafe_RESTful_API/readme.txt
+++ Day61-81/DAY66_Project-Cafe_RESTful_API/readme.txt
@@ -1,7 +1,70 @@
 ### --- PROJECT NAME / DESC --- ###
 
+RESTful API SQL Cafe DB - A small database that uses api calls to view edit add and delete cafe's from a database
+
 ### --- PARAMETERS --- ###
+
+Program must be able to access an sql database of cafes
+Must be able to route based on various api calls:
+    - random
+    - search
+    - all
+    - add
+    - edit
+    - delete
+
+must be able to manipulate the database in these ways 
 
 ### --- HOW IT WORKS --- ###
 
-### --- TO DO / FUTURE PLANS --- ### 
+Using flask and it's in built tools we create a small flask app that when an API call is made routes the user to 
+the information based on the call
+We largely do this with the use of the flask request import that allows us to take the information we type into postman / the url bar
+and feeds that back into the function
+Once we have that information we can manipulate it in a variety of ways from finding the corresponding information in the db, to adding new
+information or even deleting
+
+```
+@app.route("/random")
+def get_random_cafe():
+    cafes = db.session.query(Cafe).all()
+    random_cafe = random.choice(cafes)
+    return jsonify(cafe=random_cafe.to_dict())
+```
+
+In this example we get query the list of cafes and using a random route trigger it to search for a random cafe from the list
+we then use our in built function to_dict to turn each piece of information into a key:value pair for passing to the jsonify function
+
+
+```
+@app.route("/add", methods=["POST"])
+def add_a_cafe():
+    new_cafe = Cafe(
+        name = request.args.get("name"),
+        map_url=request.args.get("map_url"),
+        img_url=request.args.get("img_url"),
+        location=request.args.get("location"),
+        seats=request.args.get("seats"),
+        has_toilet=bool(request.args.get("toilet")),
+        has_wifi=bool(request.args.get("wifi")),
+        has_sockets=bool(request.args.get("sockets")),
+        can_take_calls=bool(request.args.get("calls")),
+        coffee_price=request.args.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    if new_cafe:
+        return jsonify(response = {"success": "Successfully added new cafe"})
+    return jsonify(response = {"failure": "New cafe could note be added"})
+```
+
+This one utilizes postman the api testing software to send a query to our url with all the parameters filled in
+we then use request.args.get to get the information we want and then create a new object for passing into the database
+
+we then also have responses to give the user feedback 
+
+
+### --- TO DO / FUTURE PLANS --- ###
+
+All in all this was a fun project and a very valuable dive into API calling and RESTful apis, this is something i'll be using when 
+going to build the professional website portfolio project.
